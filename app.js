ac8fffb7062ac05fd6b7867360e5d5b5ad3622e0
julia-francais <ju.francais@gmail.com>
2020-02-12 16:59:16 +0100

    comment beginning

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express= require('express'),
     app =express(),
     bodyParser = require ("body-parser"),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    Campground = require('./models/campground'),
+    seedDB = require('./seeds');
 
 mongoose.connect("mongodb://localhost:27017/yelp_camp", {useUnifiedTopology: true, useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
@@ -10,48 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 const port = 3000;
 
 app.set("view engine", "ejs");
-
-//SCHEMA SETUP
-const campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-const Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create(
-//     {
-//         name: "Salmon Creek", 
-//         image:"https://europe.huttopia.com/content/uploads/2016/09/Carroussel-Camping-Nature-Home.jpg",
-//         description: "Get an enchanted experience of wild wild nature at Salmon Creek"
-//     }, (err,campground) => {
-//         if(err){
-//             console.log("err", err);
-//         } else {
-//             console.log(`new campground ${campground}`);
-//         }
-//     });
-
-// const campgrounds = [
-//     {name: "Salmon Creek", image:"https://europe.huttopia.com/content/uploads/2016/09/Carroussel-Camping-Nature-Home.jpg"},
-
-//     {name: "Granite Hill", image: "https://www.dw.com/image/48396304_303.jpg"},
-
-//     {name: "Mountain Gembri", image:"https://pix10.agoda.net/hotelImages/5647641/-1/45ee96c8c2b177ffcd99818b6b6387f0.jpg?s=1024x768"},
-
-//     {name: "Salmon Creek", image:"https://europe.huttopia.com/content/uploads/2016/09/Carroussel-Camping-Nature-Home.jpg"},
-
-//     {name: "Granite Hill", image: "https://www.dw.com/image/48396304_303.jpg"},
-
-//     {name: "Mountain Gembri", image:"https://pix10.agoda.net/hotelImages/5647641/-1/45ee96c8c2b177ffcd99818b6b6387f0.jpg?s=1024x768"},
-
-//     {name: "Salmon Creek", image:"https://europe.huttopia.com/content/uploads/2016/09/Carroussel-Camping-Nature-Home.jpg"},
-
-//     {name: "Granite Hill", image: "https://www.dw.com/image/48396304_303.jpg"},
-
-//     {name: "Mountain Gembri", image:"https://pix10.agoda.net/hotelImages/5647641/-1/45ee96c8c2b177ffcd99818b6b6387f0.jpg?s=1024x768"}
-// ];
+seedDB();
 
 app.get('/', (req, res) => {
     res.render('landing');
@@ -86,12 +47,14 @@ app.get("/campgrounds/new", (req, res) => {
     res.render('new.ejs')
 });
 
+//SHOW
 app.get("/campgrounds/:id", (req, res)=> {
-    Campground.findById(req.params.id, (err,campground)=> {
+    Campground.findById(req.params.id).populate('comments').exec((err,foundCampground)=> {
         if(err){
             console.log(err);
         } else {
-            res.render('show', {campground: campground});
+            console.log(foundCampground);
+            res.render('show', {campground: foundCampground});
         }
     });
 })
