37239860d23785725f0b0f6a9440f3d13e5c6d2f
julia-francais <ju.francais@gmail.com>
2020-02-16 12:57:00 +0100

    landing page

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -18,13 +18,14 @@ router.get('/', (req, res) => {
 //CREATE - create a new campground
 router.post('/', middleware.isLoggedIn, (req,res) => {
     let name = req.body.name;
+    let price = req.body.price
     let image = req.body.image;
     let description = req.body.description;
     const author = {
         id: req.user._id,
         username: req.user.username
     };
-    var newCampground = {name: name, image: image, description: description, author: author };
+    var newCampground = {name: name, price: price, image: image, description: description, author: author };
     Campground.create(newCampground, (err,campground)=>{
         if(err){
             console.log('err', err);
