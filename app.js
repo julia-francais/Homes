144bb90353bfa985b86b0a18e044c24debb586a4
julia-francais <ju.francais@gmail.com>
2020-02-17 23:08:42 +0100

    script

--- app.js
+++ app.js
@@ -62,6 +62,6 @@ app.use(indexRoutes);
 app.use(commentRoutes);
 app.use("/campgrounds", campgroundRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Listening on ${port}`) 
 })
